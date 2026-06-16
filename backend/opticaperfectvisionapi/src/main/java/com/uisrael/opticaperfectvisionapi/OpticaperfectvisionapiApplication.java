0931b985e72ed80162fee52b6651f94965220e46
Fernanda_Ushcasina <ushcasina2508@gmail.com>
2026-06-15 21:12:54 -0500

    Version inicial api 1

---
+++ backend/opticaperfectvisionapi/src/main/java/com/uisrael/opticaperfectvisionapi/OpticaperfectvisionapiApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.opticaperfectvisionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticaperfectvisionapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticaperfectvisionapiApplication.class, args);
+	}
+
+}

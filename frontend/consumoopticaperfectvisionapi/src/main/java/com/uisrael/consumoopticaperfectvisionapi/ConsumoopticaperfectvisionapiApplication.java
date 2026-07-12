b2c0a9bb5e809ba41c61581293e54adc302f8892
Fernanda_Ushcasina <ushcasina2508@gmail.com>
2026-07-11 20:27:23 -0500

    Base proyecto MVC

---
+++ frontend/consumoopticaperfectvisionapi/src/main/java/com/uisrael/consumoopticaperfectvisionapi/ConsumoopticaperfectvisionapiApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.consumoopticaperfectvisionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoopticaperfectvisionapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoopticaperfectvisionapiApplication.class, args);
+	}
+
+}
---
+++ frontend/consumoopticaperfectvisionapi/src/test/java/com/uisrael/consumoopticaperfectvisionapi/ConsumoopticaperfectvisionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.consumoopticaperfectvisionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoopticaperfectvisionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

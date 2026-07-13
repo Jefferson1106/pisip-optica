4f68d97eed5c53fa927444bebfdef10c299474ed
Jefferson_Jitala <42380083+Jefferson1106@users.noreply.github.com>
2026-07-12 21:26:59 -0500

    fix detalle examen

--- backend/opticaperfectvisionapi/src/main/java/com/uisrael/opticaperfectvisionapi/infraestructura/repositorios/IDetalleExamenJpaRepositorio.java
+++ backend/opticaperfectvisionapi/src/main/java/com/uisrael/opticaperfectvisionapi/infraestructura/repositorios/IDetalleExamenJpaRepositorio.java
@@ -1,9 +1,13 @@
 package com.uisrael.opticaperfectvisionapi.infraestructura.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.uisrael.opticaperfectvisionapi.infraestructura.persistencia.jpa.DetalleExamenEntity;
 
 public interface IDetalleExamenJpaRepositorio extends JpaRepository<DetalleExamenEntity, Integer> {
+
+	Optional<DetalleExamenEntity> findByExamenVisual_IdExamen(Integer idExamen);
 
 }

09ba1f67157e207a015be4e3aa63ac466cf38743
Fernanda_Ushcasina <ushcasina2508@gmail.com>
2026-07-20 23:22:46 -0500

    Orden entrega Crud

---
+++ frontend/consumoopticaperfectvisionapi/src/main/java/com/uisrael/consumoopticaperfectvisionapi/services/impl/OrdenEntregaServiceImpl.java
@@ -0,0 +1,69 @@
+package com.uisrael.consumoopticaperfectvisionapi.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.uisrael.consumoopticaperfectvisionapi.model.dto.request.OrdenEntregaRequestDto;
+import com.uisrael.consumoopticaperfectvisionapi.model.dto.response.OrdenEntregaResponseDto;
+import com.uisrael.consumoopticaperfectvisionapi.services.IOrdenEntregaService;
+
+@Service
+	public class OrdenEntregaServiceImpl implements IOrdenEntregaService {
+
+	    private final WebClient webClient;
+
+	    public OrdenEntregaServiceImpl(WebClient webClient) {
+	        this.webClient = webClient;
+	    }
+
+	    @Override
+	    public List<OrdenEntregaResponseDto> listarOrdenes() {
+	        return webClient.get()
+	            .uri("/api/orden-entregas")
+	            .retrieve()
+	            .bodyToFlux(OrdenEntregaResponseDto.class)
+	            .collectList()
+	            .block();
+	    }
+
+	    @Override
+	    public void guardarOrden(OrdenEntregaRequestDto orden) {
+	        webClient.post()
+	            .uri("/api/orden-entregas")
+	            .bodyValue(orden)
+	            .retrieve()
+	            .bodyToMono(Void.class)
+	            .block();
+	    }
+
+	    @Override
+	    public OrdenEntregaResponseDto buscarPorId(Integer idEntrega) {
+	        return webClient.get()
+	            .uri("/api/orden-entregas/" + idEntrega)
+	            .retrieve()
+	            .bodyToMono(OrdenEntregaResponseDto.class)
+	            .block();
+	    }
+
+	    @Override
+	    public void actualizarOrden(Integer idEntrega, OrdenEntregaRequestDto orden) {
+	        webClient.put()
+	            .uri("/api/orden-entregas/" + idEntrega)
+	            .bodyValue(orden)
+	            .retrieve()
+	            .bodyToMono(Void.class)
+	            .block();
+	    }
+
+	    @Override
+	    public void eliminarOrden(Integer idEntrega) {
+	        webClient.delete()
+	            .uri("/api/orden-entregas/" + idEntrega)
+	            .retrieve()
+	            .bodyToMono(Void.class)
+	            .block();
+	    }
+	}
+
